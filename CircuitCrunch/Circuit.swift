41cf044106b68e258aec2521b96dd3e88368ac06
VOjha <vidushi.ojha@gmail.com>
2015-05-22 15:43:26 -0700

    fixed naming conventions on icons

---
+++ CircuitCrunch/Circuit.swift
@@ -0,0 +1,15 @@
+//
+//  Circuit.swift
+//  CircuitCrunch
+//
+//  Created by Vidushi Ojha on 5/22/15.
+//  Copyright (c) 2015 Vidushi Ojha. All rights reserved.
+//
+
+import SpriteKit {
+    
+    enum CircuitType: Int {
+        case Unknown = 0, 
+    }
+    
+}
