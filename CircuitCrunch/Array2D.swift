fe3968228d0a5a7cf5c61a4301d0a6999d00eadf
VOjha <vidushi.ojha@gmail.com>
2015-05-22 16:04:23 -0700

    added Array2D struct

---
+++ CircuitCrunch/Array2D.swift
@@ -0,0 +1,28 @@
+//
+//  Array2D.swift
+//  CircuitCrunch
+//
+//  Created by Vidushi Ojha on 5/22/15.
+//  Copyright (c) 2015 Vidushi Ojha. All rights reserved.
+//
+
+struct Array2D<T> {
+    let columns: Int
+    let rows: Int
+    private var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count: rows*columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row*columns + column]
+        }
+        set {
+            array[row*columns + column] = newValue
+        }
+    }
+}
--- CircuitCrunch/Circuit.swift
+++ CircuitCrunch/Circuit.swift
@@ -6,10 +6,52 @@
 //  Copyright (c) 2015 Vidushi Ojha. All rights reserved.
 //
 
-import SpriteKit {
+import SpriteKit
     
-    enum CircuitType: Int {
-        case Unknown = 0, 
+enum CircuitType: Int, Printable {
+    case Unknown = 0, ElectricalSensor, ElectricalThreshold, Electronics, IntegratedCircuit, MemorySlot, Robot
+    
+    var spriteName: String {
+        let spriteNames = [
+        "ElectricalSensor",
+        "ElectricalThreshold",
+        "Electronics",
+        "IntegratedCircuit",
+        "MemorySlot",
+        "Robot"]
+        
+        return spriteNames[rawValue - 1]
+    }
+    
+    var highlightedSpriteName: String {
+        return spriteName + "-Highlighted"
+    }
+    
+    static func random() -> CircuitType {
+        return CircuitType(rawValue: Int(arc4random_uniform(6)) + 1)!
+    }
+    
+    var description: String {
+        return spriteName
+    }
+}
+
+class Circuit: Printable {
+    
+    var column: Int
+    var row: Int
+    
+    let circuitType: CircuitType
+    var sprite: SKSpriteNode?
+    
+    init(column: Int, row: Int, circuitType: CircuitType) {
+        self.column = column
+        self.row = row
+        self.circuitType = circuitType
+    }
+    
+    var description: String {
+        return "type: \(circuitType) square: (\(column), \(row))"
     }
     
 }
