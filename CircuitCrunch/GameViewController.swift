89ed98044ee157e6f220c53de4051e6382819e61
VOjha <vidushi.ojha@gmail.com>
2015-05-28 10:25:47 -0700

    going to start fixing UI and graphics now

--- CircuitCrunch/GameViewController.swift
+++ CircuitCrunch/GameViewController.swift
@@ -61,7 +61,7 @@ class GameViewController: UIViewController {
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
         
-        level = Level(filename: "Level_4")
+        level = Level(filename: "Level_0")
         scene.level = level
         
         scene.addTiles()
